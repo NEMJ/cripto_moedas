bb826339a1d4bb7db04f202eeaabcac8b3b41c4b
Nelson Junior <dudu.da.mata@hotmail.com>
2022-12-11 19:11:30 -0300

    configurando projeto

---
+++ lib/pages/moedas_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MoedasPage extends StatelessWidget {
+  const MoedasPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Cripto Moedas'),
+      ),
+      body: Center(
+        child: Text('Olá'),
+      ),
+    );
+  }
+}
