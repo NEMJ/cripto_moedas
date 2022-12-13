ffebe17757459f705ffeb5f77d1e157bbe762056
Nelson Junior <dudu.da.mata@hotmail.com>
2022-12-12 21:43:26 -0300

    model, repository e listView

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,3 +1,4 @@
+import '../repositories/moeda_repository.dart';
 import 'package:flutter/material.dart';
 
 class MoedasPage extends StatelessWidget {
@@ -5,13 +6,24 @@ class MoedasPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tabela = MoedaRepository.tabela;
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Cripto Moedas'),
+        title: const Text('Cripto Moedas'),
       ),
-      body: Center(
-        child: Text('Olá'),
+      body: ListView.separated(
+        itemCount: tabela.length,
+        separatorBuilder: (_, __) => const Divider(),
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext context, int moeda) {
+          return ListTile(
+            leading: Image.asset(tabela[moeda].icone),
+            title: Text(tabela[moeda].nome),
+            trailing: Text(tabela[moeda].preco.toString()),
+          );
+        },
       ),
     );
   }
