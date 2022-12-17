74cfa4c8286852eaf977944cf973784b3e8b66bc
Nelson Junior <dudu.da.mata@hotmail.com>
2022-12-17 14:35:02 -0300

    formulario na moeda_detalhes

--- lib/pages/moeda_detalhes_page.dart
+++ lib/pages/moeda_detalhes_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 import '../models/moeda.dart';
 
 class MoedaDetalhesPage extends StatefulWidget {
@@ -11,6 +13,23 @@ class MoedaDetalhesPage extends StatefulWidget {
 }
 
 class _MoedaDetalhesPageState extends State<MoedaDetalhesPage> {
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  final _form = GlobalKey<FormState>(); // Inicializa ou instancia uma chave aleatória para o formulário
+  final _valor = TextEditingController();
+  double quantidade = 0;
+
+  comprar() {
+    if(_form.currentState!.validate()) {
+      // Salvar a compra
+
+      Navigator.pop(context);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Compra realizada com sucesso!')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,18 +37,110 @@ class _MoedaDetalhesPageState extends State<MoedaDetalhesPage> {
         centerTitle: true,
         title: Text(widget.moeda.nome),
       ),
-      body: Column(
-        children: [
-          Divider(),
-          Row(
-            children: [
-              SizedBox(
-                width: 50,
-                child: Image.asset(widget.moeda.icone),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    child: Image.asset(widget.moeda.icone),
+                    width: 50,
+                  ),
+                  Container(width: 10),
+                  Text(
+                    real.format(widget.moeda.preco),
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: -1,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+            (quantidade > 0) 
+            ? SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                margin:  EdgeInsets.only(bottom: 24),
+                // padding: EdgeInsets.all(12),
+                alignment: Alignment.center,
+                // decoration: BoxDecoration(
+                //   color: Colors.teal.withOpacity(0.05),
+                // ),
+                child: Text(
+                  '$quantidade ${widget.moeda.sigla}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+            )
+            : Container(
+              margin: EdgeInsets.only(bottom: 24),
+            ),
+            Form(
+              key: _form,
+              child: TextFormField(
+                controller: _valor,
+                style: const TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Valor',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  suffix: Text(
+                    'reais',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly], // Impede que o usuário digite letras ou símbolos no campo de texto
+                validator: (value) {
+                  if(value!.isEmpty) {
+                    return 'Informe o valor da compra';
+                  } else if(double.parse(value) < 50) {
+                    return 'Compra mínima é R\$ 50 reais';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    quantidade = (value.isEmpty)
+                    ? 0
+                    : double.parse(value) / widget.moeda.preco;
+                  });
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.only(top: 24),
+              child: ElevatedButton(
+                onPressed: comprar,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.check),
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                        'Comprar',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
